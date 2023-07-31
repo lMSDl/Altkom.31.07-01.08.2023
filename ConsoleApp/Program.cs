8b6a33d9616e5257ac71a9f10cf5ad8fdd8aafaa
Paweł Łęczycki <pawel@msd.info.pl>
2023-07-31 09:53:56 +0200

    Przygotowanie projektu

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
+var contextOptions = new DbContextOptionsBuilder<Context>()
+    .UseSqlServer(@"Server=(local)\SQLEXPRESS;Database=EFCore;Integrated security=true")
+    .Options;
+
+var context = new Context(contextOptions);
+context.Database.EnsureDeleted();
+context.Database.EnsureCreated();
+
+
---
+++ DAL/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+    }
+}
